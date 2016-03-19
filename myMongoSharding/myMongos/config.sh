2912ba8dde79f56d0521ca0a1fea622a6570a52d
AlanTai <rainman.tai@gmail.com>
2016-03-19 13:11:39 -0700

    update mongos config.sh

--- myMongoSharding/myMongos/config.sh
+++ myMongoSharding/myMongos/config.sh
@@ -1,15 +1,13 @@
 # mongos configuration
 echo "use admin" >> authentication.js
-echo "db.createUser({user:'siteUserAdmin',pwd:'shardingadmin',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.js
-echo "db.auth('siteUserAdmin', 'shardingadmin')" >> authentication.js
-echo "db.createUser({user:'siteRootAdmin',pwd:'shardingadmin',roles:[{role:'root',db:'admin'}]})" >> authentication.js
-echo "db.auth('siteRootAdmin', 'shardingadmin')" >> authentication.js
+echo "db.createUser({user:'siteUserAdmin',pwd:'EFmkEWRiowueFRWGE5625dew7Ufer567I7',roles:[{role:'userAdminAnyDatabase',db:'admin'}]})" >> authentication.js
+echo "db.auth('siteUserAdmin', 'EFmkEWRiowueFRWGE5625dew7Ufer567I7')" >> authentication.js
+echo "db.createUser({user:'siteRootAdmin',pwd:'EFmkEWRiowueFRWGE5625dew7Ufer567I7',roles:[{role:'root',db:'admin'}]})" >> authentication.js
+echo "db.auth('siteRootAdmin', 'EFmkEWRiowueFRWGE5625dew7Ufer567I7')" >> authentication.js
 echo "sleep(1000)" >> authentication.js
-echo "sh.addShard('rs1/45.56.85.129:27041,45.56.85.129:27042,45.56.85.129:27043')" >> authentication.js
+echo "sh.addShard('rs1/198.74.48.34:27030,198.74.48.34:27031,198.74.48.34:27032')" >> authentication.js
 echo "sleep(2000)" >> authentication.js
-echo "sh.addShard('rs2/45.56.85.129:27044,45.56.85.129:27045,45.56.85.129:27046')" >> authentication.js
-echo "sleep(2000)" >> authentication.js
-echo "sh.addShard('rs2/45.56.85.129:27047,45.56.85.129:27048,45.56.85.129:27049')" >> authentication.js
+echo "sh.addShard('rs2/45.33.61.89:27030,45.33.61.89:27031,45.33.61.89:27032')" >> authentication.js
 echo "sleep(2000)" >> authentication.js
 echo "db.runCommand({enablesharding: 'test'})" >> authentication.js
 echo "sleep(2000)" >> authentication.js
